1e61fc092d1573b48aa29543c58a4820e25aa72f
Jesse Windle MB <jesse.windle@gmail.com>
2015-03-03 21:45:59 -0500

    Additions to u4_deck4.R

--- Slides/04_Unit4/deck4/u4_deck4.R
+++ Slides/04_Unit4/deck4/u4_deck4.R
@@ -17,6 +17,19 @@ rxntime.example <- function(write.out=FALSE, width=400, height=300)
     lm1 = lm(PictureTarget.RT ~ as.factor(Subject), data=rxntime)
     anova(lm1)
 
+    lm2 = lm(PictureTarget.RT ~ as.factor(Subject)*as.factor(Littered), data=rxntime)
+
+    anova(lm1, lm2)
     
+    rxnt$Subject.x.Littered = paste("S", rxnt$Subject, ".", "L", rxnt$Littered, sep="")
+
+    if (write.out) png("figures/rxntime-boxplot-subject-littered.png", width=width*1.2, height=height)
+
+    boxplot(PictureTarget.RT ~ Subject.x.Littered, data=rxnt, main="Reaction times: Subject & Littered",
+            ylab="Reaction Times", xlab="", las=2)
+    mtext("Subject and Littered Status", side=1, line=4)
+    
+    if (write.out) dev.off()
+
     
 }
